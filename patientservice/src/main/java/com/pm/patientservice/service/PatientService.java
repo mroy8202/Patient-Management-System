4bd7ad27166b3792c661ffeda9aa91b08fb526d7
mritunjayRoy <mroy8202@gmail.com>
2025-04-27 18:41:34 +0530

    create patient endpoint and custom exceptions added

--- patientservice/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patientservice/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -1,6 +1,8 @@
 package com.pm.patientservice.service;
 
+import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.exception.EmailAlreadyExistsException;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
@@ -10,7 +12,7 @@ import java.util.List;
 
 @Service
 public class PatientService {
-    private PatientRepository patientRepository;
+    private final PatientRepository patientRepository;
 
     // constructor
     public PatientService(PatientRepository patientRepository) {
@@ -29,5 +31,20 @@ public class PatientService {
 
         return patients.stream()
                 .map(PatientMapper::toDTO).toList();
+    }
+
+    public PatientResponseDTO createPatients(PatientRequestDTO patientRequestDTO) {
+
+        if(patientRepository.existsByEmail(patientRequestDTO.getEmail())) {
+            throw new EmailAlreadyExistsException(
+                    "A Patient with this email already exists: " + patientRequestDTO.getEmail()
+            );
+        }
+
+        Patient newPatient = patientRepository.save(
+                PatientMapper.toModel(patientRequestDTO)
+        );
+
+        return PatientMapper.toDTO(newPatient);
     }
 }
